81f67878d90eab75b1fa050d94096f1ca483227c
Héctor Mosquera Turner <hmosqueraturner@gmail.com>
2023-07-14 15:29:26 +0200

    Update AcidJavaApp.java

--- src/main/java/com/imoto/acid/acidJavaApp/AcidJavaApp.java
+++ src/main/java/com/imoto/acid/acidJavaApp/AcidJavaApp.java
@@ -1,7 +1,20 @@
+package com.imoto.acid.acidJavaApp;
+
+/**
+ * Hello world!
+ */
 
 class AcidJavaApp
 {
-  public static void main(String args[]){  
-       System.out.println("Hello Java");  
-  }
+private static final String MESSAGE = "Hello ACiD World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
 }
